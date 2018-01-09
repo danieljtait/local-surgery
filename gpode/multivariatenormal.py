8486c9145618ee6c50c7d36e6295c1dc1579de8d
Leinad <leinad@Daniels-MacBook-Pro.local>
2018-01-09 08:35:20 +0000

    Sim function added to multivariate normal

--- gpode/multivariatenormal.py
+++ gpode/multivariatenormal.py
@@ -11,3 +11,16 @@ def CondPar(x2, m1, m2, C11, C12, C22):
     condMean = m1 + np.dot(C12, np.linalg.solve(L.T, np.linalg.solve(L, x2-m2)))
     condCov = C11 - np.dot(C12, np.linalg.solve(L.T, np.linalg.solve(L, C12.T)))
     return condMean, condCov
+
+
+####
+# Simulates a single sample from the multivariate
+# normal distribution
+def Sim(mean, cov=None, L=None):
+    # Looking for the Cholesky decomposition to have been passed
+    # as an argument
+    if L:
+        return mean + np.dot(L, np.random.normal(size=L.shape[0]))
+    else:
+        L = np.linalg.cholesky(cov)
+        return Sim(mean, L=L)
