7410ba60a40e00419834d52a5f075c7642bcb1ed
Leinad <leinad@Daniels-MacBook-Pro.local>
2018-01-18 16:23:02 +0000

    gaussian process class definitions

---
+++ gpode/gaussianprocesses/gaussianprocesses.py
@@ -0,0 +1,66 @@
+import gpode.kernels.kernels as kernels
+
+
+class GaussianProcess:
+    def __init__(self, kernel, inputs=None):
+
+        if isinstance(kernel, kernels.Kernel):
+            self.kernel = kernel
+        else:
+            raise ValueError
+
+        self.inputs = inputs
+
+        # store Cholesky decomp. of covar mat during fitting
+        self.L = None
+
+
+class MultioutputGaussianProcess(GaussianProcess):
+    def __init__(self, mokernel, inputs=None):
+
+        if isinstance(mokernel, kernels.MultioutputKernel):
+            super(MultioutputGaussianProcess, self).__init__(mokernel, inputs)
+
+        # Initalisation failed
+        else:
+            raise ValueError
+
+    # Will usually be overridden in subclasses
+    def fit(self, outputs):
+        self._fit(outputs)
+
+    ##
+    # Puts the model in a position to return predictive
+    # distributions as desired
+    def _fit(self, outputs):
+        _outputs_w_data = []  # Which outputs have been fitted
+        for ind, data in outputs:
+            _outputs_w_data.append(ind)
+            self.fitted_outputs[ind] = data
+
+        _outputs_w_data = sorted(_outputs_w_data)
+        for i in _outputs_w_data:
+            for j in _outputs_w_data:
+                self.fit_cov(i, j)
+            _outputs_w_data.remove(i)
+
+
+def gradgp_fit(func):
+    def func_wrapper(self, *args, **kwargs):
+        data = []
+        for ind, key in zip((0, 1), ("x", "dx")):
+            try:
+                data.append((ind, kwargs[key]))
+            except:
+                pass
+        self._fit(data)
+    return func_wrapper
+
+
+class GradientGaussianProcess(MultioutputGaussianProcess):
+    def __init__(self, kernel, inputs=None):
+        super(GradientGaussianProcess, self).__init__(kernel, inputs)
+
+    @gradgp_fit
+    def fit(self, x=None, dx=None):
+        pass
