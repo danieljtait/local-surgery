0ce12aac8e839cdd8e8ab3f35abb0d4ddafeedd1
danieljtait <tait.djk@gmail.com>
2018-02-16 17:17:33 +0000

    Variational fit of mlfm

--- gpode/latentforcemodels/__init__.py
+++ gpode/latentforcemodels/__init__.py
@@ -2,3 +2,4 @@ from .nestedgpintegrals import *
 from .mlfm_ladap_grad import MulLatentForceModel_adapgrad
 from .mlfmneumanngenerative import NestedIntegralKernel, NeumannGenerativeModel
 from .mlfmvariational import VariationalMLFM
+from .mlfmvar_ns import VariationalMLFM2
---
+++ gpode/latentforcemodels/mlfmvar_ns.py
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy.stats import norm
+from scip.linalg import block_diag
+
+
+class VariationalMLFM2:
+
+    def __init__(self):
+        pass
